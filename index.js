1e82e77bb2af2bb54f7a66a89c7f204dd3d2d40c
cruciaal <vitaliimarchenko@rocketmail.com>
2023-04-17 16:47:13 +0300

    123

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-'use strict';
-
 export const tasks = [
   { text: 'Buy milk', done: false },
   { text: 'Pick up Tom from airport', done: false },
@@ -36,4 +34,44 @@ const renderTasks = (tasksList) => {
   // list.replaceChildren(...listItems);
   list.replaceChildren(...listItems);
 };
+
 renderTasks(tasks);
+// 'use strict';
+
+// export const tasks = [
+//   { text: 'Buy milk', done: false },
+//   { text: 'Pick up Tom from airport', done: false },
+//   { text: 'Visit party', done: false },
+//   { text: 'Visit doctor', done: true },
+//   { text: 'Buy meat', done: true },
+// ];
+
+// /**
+//  * @param {object[]} tasksList
+//  * @return {undefined}
+//  */
+// const renderTasks = (tasksList) => {
+//   const listItems = tasksList.map((elelm) => {
+//     //li .list__item if done = true ==> .list__item_done
+//     //   console.log(task);
+//     const done = elelm.done ? 'list__item_done' : '';
+//     const text = elelm.text;
+//     const listItem = document.createElement('li');
+//     listItem.setAttribute('class', `list__item ${done}`);
+
+//     //input type="checkbox" class="list__item-checkbox
+//     const inputCheckbox = document.createElement('input');
+//     inputCheckbox.setAttribute('class', 'list__item-checkbox');
+//     inputCheckbox.setAttribute('type', 'checkbox');
+//     inputCheckbox.checked = !!done;
+
+//     // append inputCheckbox, into listItem
+//     listItem.append(inputCheckbox, text);
+//     return listItem;
+//   });
+//   // add tasks to list
+//   const list = document.querySelector('.list');
+//   // list.replaceChildren(...listItems);
+//   list.replaceChildren(...listItems);
+// };
+// renderTasks(tasks);
